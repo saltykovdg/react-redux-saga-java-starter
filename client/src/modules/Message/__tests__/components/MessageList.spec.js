3f5a61ca9fc3840a79a97f459c6bd4c48a87d09a
Dmitry Saltykov <d.saltykov@yandex.ru>
2016-10-23 16:26:46 +0800

    added test for MessageList

--- client/src/modules/Message/__tests__/components/MessageList.spec.js
+++ client/src/modules/Message/__tests__/components/MessageList.spec.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { FormattedMessage } from 'react-intl';
+import MessageList from '../../components/MessageList';
+
+const message1 = { text: 'Message 1', use_intl: false };
+const message2 = { text: 'Message 2', use_intl: false };
+const messages = [message1, message2];
 
 describe('renders the message list properly', () => {
-  it('contains spec with an expectation', () => {
-    expect(true).toBe(true);
+  const wrapper = shallow(<MessageList messages={messages} />);
+  it('should contains ul element', () => {
+    expect(wrapper.find('ul').length).toBe(1);
+    expect(wrapper.find('ul').hasClass('messages')).toBe(true);
+  });
+  it('should contains h4 element', () => {
+    expect(wrapper.find('h4').length).toBe(1);
+  });
+  it('should contains FormattedMessage with id messageListHeader (in h4)', () => {
+    expect(wrapper.find('h4').containsMatchingElement(<FormattedMessage id="messageListHeader" />)).toBe(true);
+  });
+  it('should contains MessageListItem', () => {
+    expect(wrapper.find('MessageListItem').length).toBe(2);
   });
 });
