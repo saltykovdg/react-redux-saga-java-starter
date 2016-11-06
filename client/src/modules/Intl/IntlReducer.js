8f3ecd1c8b6937aa79afcad7013497f146577c77
Dmitry Saltykov <d.saltykov@yandex.ru>
2016-11-07 06:15:00 +0800

    fix intl

--- client/src/modules/Intl/IntlReducer.js
+++ client/src/modules/Intl/IntlReducer.js
@@ -1,7 +1,7 @@
 import { enabledLanguages, localizationData } from '../../Intl/setup';
 import { SWITCH_LANGUAGE } from './IntlActions';
 
-const initLocale = (global.navigator && global.navigator.language) || 'en';
+const initLocale = ((global.navigator && global.navigator.language) || 'en').substring(0, 2);
 
 const initialState = {
   locale: initLocale,
